07e32b3122695afeb7ccd967e005dc92cc1f96ab
Paul Singh <paul21479@gmail.com>
2022-11-14 15:03:17 +1300

    Added few components, spelling error fix, added strictmode, and remove comment in index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React  from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'
+import App from './App';
 import './index.css';
 // import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
